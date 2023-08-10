c83217f948b71db03d13c02fcbcf00211f883a35
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-10 17:30:57 +0100

    transient error retry on search service

--- Carsties/src/SearchService/Program.cs
+++ Carsties/src/SearchService/Program.cs
@@ -1,7 +1,6 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -30,3 +29,7 @@ catch (Exception ex)
 }
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions.HandleTransientHttpError()
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
