632eb2acb4805be557ae2a846111ea3e56aeb2ad
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-14 09:39:55 +0100

    introduce outbox persistence to AuctionService MassTransit

--- Carsties/src/AuctionService/Program.cs
+++ Carsties/src/AuctionService/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
