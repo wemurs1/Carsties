cd1ed7f6f0d64d1d8a3a4b85f1b962da073861a5
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-16 16:01:02 +0100

    add remaaining message consumers and identity client config

---
+++ Carsties/src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming Auction Finished");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (auction == null)
+        {
+            Console.WriteLine("Auction not found: " + context.Message.AuctionId);
+            return;
+        }
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
---
+++ Carsties/src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (auction == null)
+        {
+            Console.WriteLine("Auction not found: " + context.Message.AuctionId);
+            return;
+        }
+
+        if (context.Message.BidStatus!.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
