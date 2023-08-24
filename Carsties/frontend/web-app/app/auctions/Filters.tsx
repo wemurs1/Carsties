13aae0875f58fac0e490313e8836af3dc7303ee4
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-24 09:21:07 +0100

    pagination

---
+++ Carsties/frontend/web-app/app/auctions/Filters.tsx
@@ -0,0 +1,32 @@
+import { Button } from 'flowbite-react';
+import ButtonGroup from 'flowbite-react/lib/esm/components/Button/ButtonGroup';
+import React from 'react';
+
+type Props = {
+  pageSize: number;
+  setPageSize: (size: number) => void;
+};
+
+const pageSizeButtons = [4, 8, 12];
+
+export default function Filters({ pageSize, setPageSize }: Props) {
+  return (
+    <div className='flex justify-between items-center mb-4'>
+      <div>
+        <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
+        <ButtonGroup>
+          {pageSizeButtons.map((value, index) => (
+            <Button
+              key={index}
+              onClick={() => setPageSize(value)}
+              color={`${pageSize === value ? 'red' : 'gray'}`}
+              className='focus:ring-0'
+            >
+              {value}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </div>
+    </div>
+  );
+}
--- Carsties/frontend/web-app/next.config.js
+++ Carsties/frontend/web-app/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { serverActions: true },
   images: {
     domains: ['cdn.pixabay.com'],
   },
