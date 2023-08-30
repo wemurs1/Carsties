f81564aee68baef43f2aef1c7dcde6a30d0cf5a2
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-30 13:53:07 +0100

    client auth

--- Carsties/frontend/web-app/app/session/page.tsx
+++ Carsties/frontend/web-app/app/session/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { getSession } from '../actions/authActions';
+import { getSession, getTokenWorkaround } from '../actions/authActions';
 import Heading from '../components/Heading';
+import AuthTest from './AuthTest';
 
 export default async function Session() {
   const session = await getSession();
+  const token = await getTokenWorkaround();
 
   return (
     <div>
@@ -11,6 +13,13 @@ export default async function Session() {
       <div className='bg-blue-200 border-2 border-blue-500'>
         <h3 className='text-lg'>Session data</h3>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+      <div className='mt-4'>
+        <AuthTest />
+      </div>
+      <div className='bg-green-200 border-2 border-green-500 mt-4'>
+        <h3 className='text-lg'>Token data</h3>
+        <pre className='overflow-auto'>{JSON.stringify(token, null, 2)}</pre>
       </div>
     </div>
   );
