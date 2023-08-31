f110392995627b354266e3a1de3c14f77b3dc2e7
Stuart Murray <stuart.murray.dev@outlook.com>
2023-08-31 17:54:21 +0100

    outline create auction and add react-hook-form and datepicker

---
+++ Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AuctionForm() {
+    return (
+        <div>Auction form goes here</div>
+    )
+}
--- Carsties/frontend/web-app/app/auctions/create/page.tsx
+++ Carsties/frontend/web-app/app/auctions/create/page.tsx
@@ -1,5 +1,10 @@
+import Heading from '@/app/components/Heading';
 import React from 'react';
+import AuctionForm from '../AuctionForm';
 
 export default function Create() {
-  return <div>Create auction</div>;
+  return <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
+    <Heading title='Sell you car!' subTitle='Please enter the details of your car' />
+    <AuctionForm />
+  </div>;
 }
