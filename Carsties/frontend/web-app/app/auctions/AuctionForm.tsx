bd7adfd534388a8fab9598b47e1135c882ffe567
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-01 16:15:46 +0100

    create auction form and fetchWrapper

--- Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
+++ Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,7 +1,106 @@
-import React from 'react'
+"use client";
+
+import { Button, TextInput } from "flowbite-react";
+import React, { useEffect } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
+import DateInput from "../components/DateInput";
 
 export default function AuctionForm() {
-    return (
-        <div>Auction form goes here</div>
-    )
+  const {
+    control,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid, isDirty, errors },
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  useEffect(() => {
+    setFocus("make");
+  }, [setFocus]);
+
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
+
+  return (
+    <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        label='Make'
+        name='make'
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label='Model'
+        name='model'
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label='Color'
+        name='color'
+        control={control}
+        rules={{ required: "Color is required" }}
+      />
+
+      <div className='grid grid-cols-2 gap-3'>
+        <Input
+          label='Year'
+          name='year'
+          control={control}
+          rules={{ required: "Year is required" }}
+          type='number'
+        />
+        <Input
+          label='Mileage'
+          name='mileage'
+          control={control}
+          rules={{ required: "Mileage is required" }}
+          type='number'
+        />
+      </div>
+
+      <Input
+        label='Image URL'
+        name='imageUrl'
+        control={control}
+        rules={{ required: "Image URL is required" }}
+      />
+
+      <div className='grid grid-cols-2 gap-3'>
+        <Input
+          label='Reserve Price (enter 0 if no reserve)'
+          name='reservePrice'
+          control={control}
+          rules={{ required: "Reserve price is required" }}
+          type='number'
+        />
+        <DateInput
+          label='Auction end date/time'
+          name='auctionEnd'
+          control={control}
+          dateFormat='dd MMM yyyy h:mm a'
+          showTimeSelect
+          rules={{ required: "Auction end date is required" }}
+        />
+      </div>
+
+      <div className='flex justify-between'>
+        <Button outline color='gray'>
+          Cancel
+        </Button>
+        <Button
+          isProcessing={isSubmitting}
+          // disabled={!isValid}
+          type='submit'
+          outline
+          color='success'
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
 }
--- Carsties/frontend/web-app/app/auctions/create/page.tsx
+++ Carsties/frontend/web-app/app/auctions/create/page.tsx
@@ -1,10 +1,15 @@
-import Heading from '@/app/components/Heading';
-import React from 'react';
-import AuctionForm from '../AuctionForm';
+import Heading from "@/app/components/Heading";
+import React from "react";
+import AuctionForm from "../AuctionForm";
 
 export default function Create() {
-  return <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
-    <Heading title='Sell you car!' subTitle='Please enter the details of your car' />
-    <AuctionForm />
-  </div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subTitle="Please enter the details of your car"
+      />
+      <AuctionForm />
+    </div>
+  );
 }
