a44f168da5087ef61720964a3a9b29420d003ffd
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-03 15:46:38 +0100

    create/update auction

--- Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
+++ Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
@@ -5,30 +5,52 @@ import React, { useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import Input from "../components/Input";
 import DateInput from "../components/DateInput";
-import { createAuction } from "../actions/auctionActions";
-import { useRouter } from "next/navigation";
+import { createAuction, updateAuction } from "../actions/auctionActions";
+import { usePathname, useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
+import { Auction } from "@/types";
 
-export default function AuctionForm() {
+type Props = {
+  auction?: Auction;
+};
+
+export default function AuctionForm({ auction }: Props) {
   const router = useRouter();
+  const pathname = usePathname();
   const {
     control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
+    reset,
   } = useForm({
     mode: "onTouched",
   });
 
   useEffect(() => {
+    if (auction) {
+      const { make, model, mileage, color, year } = auction;
+      reset({ make, model, mileage, color, year });
+    }
     setFocus("make");
   }, [setFocus]);
 
   async function onSubmit(data: FieldValues) {
     try {
-      const res = await createAuction(data);
+      let id = "";
+      let res;
+      if (pathname === "/auctions/create") {
+        res = await createAuction(data);
+        id = res.id;
+      } else {
+        if (auction) {
+          res = await updateAuction(data, auction.id);
+          id = auction.id;
+        }
+      }
+
       if (res.error) throw res.error;
-      router.push(`/auctions/details/${res.id}`);
+      router.push(`/auctions/details/${id}`);
     } catch (error: any) {
       toast.error(error.status + " " + error.message);
     }
@@ -72,30 +94,34 @@ export default function AuctionForm() {
         />
       </div>
 
-      <Input
-        label='Image URL'
-        name='imageUrl'
-        control={control}
-        rules={{ required: "Image URL is required" }}
-      />
+      {pathname === "/auctions/create" && (
+        <>
+          <Input
+            label='Image URL'
+            name='imageUrl'
+            control={control}
+            rules={{ required: "Image URL is required" }}
+          />
 
-      <div className='grid grid-cols-2 gap-3'>
-        <Input
-          label='Reserve Price (enter 0 if no reserve)'
-          name='reservePrice'
-          control={control}
-          rules={{ required: "Reserve price is required" }}
-          type='number'
-        />
-        <DateInput
-          label='Auction end date/time'
-          name='auctionEnd'
-          control={control}
-          dateFormat='dd MMM yyyy h:mm a'
-          showTimeSelect
-          rules={{ required: "Auction end date is required" }}
-        />
-      </div>
+          <div className='grid grid-cols-2 gap-3'>
+            <Input
+              label='Reserve Price (enter 0 if no reserve)'
+              name='reservePrice'
+              control={control}
+              rules={{ required: "Reserve price is required" }}
+              type='number'
+            />
+            <DateInput
+              label='Auction end date/time'
+              name='auctionEnd'
+              control={control}
+              dateFormat='dd MMM yyyy h:mm a'
+              showTimeSelect
+              rules={{ required: "Auction end date is required" }}
+            />
+          </div>
+        </>
+      )}
 
       <div className='flex justify-between'>
         <Button outline color='gray'>
