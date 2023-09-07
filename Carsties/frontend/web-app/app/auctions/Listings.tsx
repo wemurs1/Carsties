83b37e5a5ab3cecbf3bf2b091c351e472da2a2e1
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-07 09:29:17 +0100

    move auctions to a store

--- Carsties/frontend/web-app/app/auctions/Listings.tsx
+++ Carsties/frontend/web-app/app/auctions/Listings.tsx
@@ -1,18 +1,19 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import AuctionCard from './AuctionCard';
-import AppPagination from '../components/AppPagination';
-import { getData } from '../actions/auctionActions';
-import { Auction, PagedResult } from '@/types';
-import Filters from './Filters';
-import { useParamsStore } from '@/hooks/useParamsStore';
-import { shallow } from 'zustand/shallow';
-import qs from 'query-string';
-import EmptyFilter from '../components/EmptyFilter';
+import React, { useEffect, useState } from "react";
+import AuctionCard from "./AuctionCard";
+import AppPagination from "../components/AppPagination";
+import { getData } from "../actions/auctionActions";
+import { Auction, PagedResult } from "@/types";
+import Filters from "./Filters";
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { shallow } from "zustand/shallow";
+import qs from "query-string";
+import EmptyFilter from "../components/EmptyFilter";
+import { useAuctionStore } from "@/hooks/useAuctionStore";
 
 export default function Listings() {
-  const [data, setData] = useState<PagedResult<Auction>>();
+  const [loading, setLoading] = useState(true);
   const params = useParamsStore(
     (state) => ({
       pageNumber: state.pageNumber,
@@ -21,12 +22,21 @@ export default function Listings() {
       orderBy: state.orderBy,
       filterBy: state.filterBy,
       seller: state.seller,
-      winner: state.winner
+      winner: state.winner,
     }),
     shallow
   );
+  const data = useAuctionStore(
+    (state) => ({
+      auctions: state.auctions,
+      totalCount: state.totalCount,
+      pageCount: state.pageCount,
+    }),
+    shallow
+  );
+  const setData = useAuctionStore((state) => state.setData);
   const setParams = useParamsStore((state) => state.setParams);
-  const url = qs.stringifyUrl({ url: '', query: params });
+  const url = qs.stringifyUrl({ url: "", query: params });
 
   function setPageNumber(pageNumber: number) {
     setParams({ pageNumber });
@@ -35,10 +45,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then((data) => {
       setData(data);
+      setLoading(false);
     });
   }, [url]);
 
-  if (!data) return <h3>Loading...</h3>;
+  if (loading) return <h3>Loading...</h3>;
 
   return (
     <>
@@ -48,7 +59,7 @@ export default function Listings() {
       ) : (
         <>
           <div className='grid grid-cols-4 gap-6'>
-            {data.results.map((auction) => (
+            {data.auctions.map((auction) => (
               <AuctionCard auction={auction} key={auction.id} />
             ))}
           </div>
