0fa49b55dffdb6d8ee1f6730f74fae74b7f8aeec
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-07 08:51:25 +0100

    dockerise notificiation service

---
+++ Carsties/src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("==> Bid placed messge received");
+        await _hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+    }
+}
