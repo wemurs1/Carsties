bf43c8f4111845a29e926c19ca80b3ed41c36257
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-08 19:11:22 +0100

    fix useEffect dependancies

--- Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
+++ Carsties/frontend/web-app/app/auctions/AuctionForm.tsx
@@ -33,7 +33,7 @@ export default function AuctionForm({ auction }: Props) {
       reset({ make, model, mileage, color, year });
     }
     setFocus("make");
-  }, [setFocus]);
+  }, [setFocus, auction, reset]);
 
   async function onSubmit(data: FieldValues) {
     try {
