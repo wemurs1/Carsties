74f34974615e6d9233ed4415589f75c6ebd56334
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-19 12:34:56 +0100

    complete code coverage of tests

--- Carsties/tests/AuctionService.UnitTests/AuctionEntityTests.cs
+++ Carsties/tests/AuctionService.UnitTests/AuctionEntityTests.cs
@@ -16,4 +16,17 @@ public class AuctionEntityTests
         // assert
         Assert.True(result);
     }
+
+    [Fact]
+    public void HasReservePrice_ReservePriceIsZero_False()
+    {
+        // arrange
+        var auction = new Auction { Id = Guid.NewGuid(), ReservePrice = 0 };
+
+        // act
+        var result = auction.HasReservePrice();
+
+        // assert
+        Assert.False(result);
+    }
 }
