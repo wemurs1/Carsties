5754dbb06a9a5af0399dab574280f75d05aa7f6d
Stuart Murray <stuart.murray.dev@outlook.com>
2023-09-20 09:44:38 +0100

    create UserPrincipal and test CreateAuction

--- Carsties/tests/AuctionService.UnitTests/AuctionControllerTests.cs
+++ Carsties/tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -1,12 +1,16 @@
 using AuctionService.Controllers;
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AuctionService.RequestHelpers;
+using AuctionService.UnitTests.Utils;
 using AutoFixture;
 using AutoMapper;
 using MassTransit;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using NuGet.Frameworks;
 
 namespace AuctionService.UnitTests;
 
@@ -29,7 +33,16 @@ public class AuctionControllerTests
             mc.AddMaps(typeof(MappingProfiles).Assembly);
         }).CreateMapper().ConfigurationProvider;
         _mapper = new Mapper(mockMapper);
-        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = Helpers.GetClaimsPrincipal()
+                }
+            }
+        };
     }
 
     [Fact]
@@ -74,4 +87,74 @@ public class AuctionControllerTests
         // assert
         Assert.IsType<NotFoundResult>(result.Result);
     }
+
+    [Fact]
+    public async Task CreateAuction_WithValidCreateAuctionDto_ReturnCreatedAtAction()
+    {
+        // arrange
+        var auction = _fixture.Create<CreateAuctionDto>();
+        _auctionRepo.Setup(repo => repo.AddAuction(It.IsAny<Auction>()));
+        _auctionRepo.Setup(repo => repo.SaveChangesAsync()).ReturnsAsync(true);
+
+        // act
+        var result = await _controller.CreateAuction(auction);
+        var createdResult = result.Result as CreatedAtActionResult;
+
+        // assert
+        Assert.NotNull(createdResult);
+        Assert.Equal("GetAuctionById", createdResult.ActionName);
+        Assert.IsType<AuctionDto>(createdResult.Value);
+    }
+
+    [Fact]
+    public async Task CreateAuction_FailedSave_Returns400BadRequest()
+    {
+        // arrange
+        var auction = _fixture.Create<CreateAuctionDto>();
+        _auctionRepo.Setup(repo => repo.AddAuction(It.IsAny<Auction>()));
+        _auctionRepo.Setup(repo => repo.SaveChangesAsync()).ReturnsAsync(false);
+
+        // act
+        var result = await _controller.CreateAuction(auction);
+        var createdResult = result.Result as CreatedAtActionResult;
+
+        // assert
+        Assert.IsType<BadRequestObjectResult>(result.Result);
+    }
+
+    // [Fact]
+    // public async Task UpdateAuction_WithUpdateAuctionDto_ReturnsOkResponse()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // [Fact]
+    // public async Task UpdateAuction_WithInvalidUser_Returns403Forbid()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // [Fact]
+    // public async Task UpdateAuction_WithInvalidGuid_ReturnsNotFound()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // [Fact]
+    // public async Task DeleteAuction_WithValidUser_ReturnsOkResponse()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // [Fact]
+    // public async Task DeleteAuction_WithInvalidGuid_Returns404Response()
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    // [Fact]
+    // public async Task DeleteAuction_WithInvalidUser_Returns403Response()
+    // {
+    //     throw new NotImplementedException();
+    // }
 }
